aafb20dfde32eaefbdd28df6565afd161947c62d
Ahmed <abumeis1991@gmail;COM>
2021-03-29 15:43:34 +0200

    navabr

--- frontend/lbp/src/views/Cities.jsx
+++ frontend/lbp/src/views/Cities.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
-class Products extends Component {
+class Cities extends Component {
   render() {
     return (
       <div>
-        <h1>Products</h1>
+        <h1>Cities</h1>
       </div>
     );
   }
 }
 
-export default Products;
+export default Cities;
---
+++ frontend/lbp/src/views/Error404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Error404 extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center", paddingTop: "46px" }}>Error 404</h1>
+      </div>
+    );
+  }
+}
+
+export default Error404;
