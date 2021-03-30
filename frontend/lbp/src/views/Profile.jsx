d2fe9bb2784e48c02ca056ec455fdb438d2b3b68
Ahmed <abumeis1991@gmail;COM>
2021-03-30 15:10:41 +0200

    de Ahmed

--- frontend/lbp/src/views/Profile.jsx
+++ frontend/lbp/src/views/Profile.jsx
@@ -1,13 +1,98 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
 
-class Profile extends Component {
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isConnected: false,
+    };
+  }
+
+  //componentDidMount() {
+  //  fetch("", {
+  //    headers: {
+  //      authorization: "Bearer " + localStorage.getItem("token"),
+  //    },
+  //  })
+  //    .then((response) => {
+  //      console.log(response.status);
+  //
+  //      if (response.status === 403) {
+  //        this.setState({ isConnected: false });
+  //      } else {
+  //        this.setState({ isConnected: true });
+  //      }
+  //      return response.json();
+  //    })
+  //    .then((response) => {
+  //      this.setState({ users: response.data });
+  //      console.log(this.state.users);
+  //    })
+  //    .catch((error) => {
+  //      this.setState({ isConnected: false });
+  //      console.log(error);
+  //    });
+  //}
+
   render() {
-    return (
-      <div>
-        <h1>Profile</h1>
-      </div>
-    );
+    if (!this.state.isConnected) {
+      return <h1>Please login first</h1>;
+    }
+
+    if (this.state.users && this.state.users.length > 0) {
+      return (
+        <>
+          <h2 style={{ textAlign: "center", color: "blue" }}>
+            This all our users that they are regestred in our plateforme{" "}
+          </h2>
+
+          <>
+            {this.state.users.map((user) => {
+              return (
+                <div className="row pt-5" style={{ justifyContent: "center" }}>
+                  <Card
+                    style={{
+                      justifyContent: "center",
+                      width: 500,
+                      color: "red",
+                    }}
+                    className={{ maxWidth: 345 }}
+                  >
+                    <CardActionArea>
+                      <CardMedia
+                        style={{ height: 300 }}
+                        image={`https://itobibe.herokuapp.com${user.profilePic}`}
+                        title="Contemplative Reptile"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          First name :{user.firstName}
+                        </Typography>
+                        <Typography gutterBottom variant="h5" component="h3">
+                          Last name :{user.surName}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </div>
+              );
+            })}
+          </>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            Back to HomePage
+          </Link>
+        </>
+      );
+    }
+
+    return <div>Aucun users</div>;
   }
 }
-
 export default Profile;
