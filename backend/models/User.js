380f4850834caa13563f2ae020e329bb436865ca
sauravBarua <myallmailofficial@gmail.com>
2021-03-31 08:46:32 +0200

    Add product schema in user-js file

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,4 +24,29 @@ const userSchema = new mongoose.Schema({
 });
 const UserModel = mongoose.model('User', userSchema);
 
+//Product schema
+
+const productSchema = new mongoose.Schema({
+    creator: { 
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+      },
+    name: {
+        type: String,
+        required: true
+    },
+    
+    price: {
+        type: Number,
+        required: true
+    },
+    Description: {
+        type: String,
+        required: true
+    },
+   
+    productPicture: String
+});
+const ProductModel = mongoose.model('Product', productSchema);
+
 module.exports = UserModel;
